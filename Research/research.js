50581ced53d83302882d7a02a95eddeace42f71c
jackhamondjmj <160562913+jackhamondjmj@users.noreply.github.com>
2024-12-11 15:36:39 -0600

    Update research.js

--- Research/research.js
+++ Research/research.js
@@ -1,16 +1,16 @@
-const toggleElements = document.querySelectorAll('.research-category');
+const buttons = document.querySelectorAll('.dropdown-toggle');
 
-        toggleElements.forEach(element => {
-            element.addEventListener('click', function() {
-                const subCategory = document.getElementById(element.id + 'Subcategories');
+        buttons.forEach(button => {
+            button.addEventListener('click', function() {
+                const subCategory = document.getElementById(button.id.replace('Button', 'Subcategories'));
                 subCategory.style.display = (subCategory.style.display === 'none' || subCategory.style.display === '') ? 'block' : 'none';
-                element.classList.toggle('open');
+                button.classList.toggle('open');
             });
 
             // Open the dropdown by default
-            const subCategory = document.getElementById(element.id + 'Subcategories');
+            const subCategory = document.getElementById(button.id.replace('Button', 'Subcategories'));
             if (subCategory) {
                 subCategory.style.display = 'block';  // Ensure it is open by default
-                element.classList.add('open');  // Add the open indicator
+                button.classList.add('open');  // Add the open indicator
             }
         });

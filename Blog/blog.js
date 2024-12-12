d82c68aa127944ce92a8e36d862cc0c014b71663
jackhamondjmj <160562913+jackhamondjmj@users.noreply.github.com>
2024-12-12 11:54:58 -0600

    Delete Blog/blog.js

--- Blog/blog.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  // Toggle the month list when a year button is clicked
-  $(".dropdown-toggle").click(function() {
-    $(this).next(".month-list").slideToggle(); // Slide down the next <ul> (month list)
-  });
-});
